e3f80a12541433ec28fc8dac935d5bf670047f28
ShipOfNik <SemenovMix@mail.ru>
2021-09-18 15:53:04 +0300

    lesson_2

--- Lesson_2/Task_1.py
+++ Lesson_2/Task_1.py
@@ -4,7 +4,17 @@
 # 15 // 2
 # 15 ** 2
 
-print(15*3, type(15*3))
+print(f"\n\n{'-' * 50} Вар1\n")
+
+examples = [15 * 3, 15 / 3, 15 // 2, 15 ** 2]
+
+for i in examples:
+    print(i, type(i))
+
+
+print(f"\n\n{'-' * 50} Вар2\n")
+
+print(15 * 3, type(15 * 3))
 print(15 / 3, type(15 / 3))
 print(15 // 2, type(15 // 2))
 print(15 ** 2, type(15 ** 2))
